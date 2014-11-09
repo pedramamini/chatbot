3ce7f55642abe4bb0e5bbbe99f701647fdedba92
Pedram Amini <pedram.amini@gmail.com>
2014-11-09 13:35:46 -0600

    adding some personality scaffolding

---
+++ handlers/personality.py
@@ -0,0 +1,35 @@
+import random
+import requests
+import simplejson
+
+LS_OUT = """
+pedram@Pedbook:~/Projects/Jumpshot/chatbot on master
+$ ls
+Procfile            config.py           go.sh               handlers            helpers.pyc
+hipchat.pyc         local.config.py     pg-tickle.py        runtime.txt         venv
+README.md           config.pyc          goped.sh            helpers.py          hipchat.py
+jumpbot.py          officer_pete.memory requirements.txt    sample.config.py
+""".strip()
+
+
+########################################################################################################################
+class handler:
+    """
+    Starting the scaffolding for building out a memory backed (sql) personality engine.
+    """
+
+    ####################################################################################################################
+    def __init__ (self, bot):
+        self.bot = bot
+
+        # register triggers.
+        self.bot.register_trigger(self.ls_detected, "regex", "(?i).*(ls -l).*")
+
+
+    ####################################################################################################################
+    def ls_detected (self, xmpp_message, room, nick, message):
+        """
+        Super basic retort.
+        """
+
+        return LS_OUT
